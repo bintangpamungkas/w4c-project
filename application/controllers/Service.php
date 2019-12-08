a64021dfa35f8f665dfe3d1c05c921a9cc309c18
ranggasatriaa <ranggasatriaa@gmail.com>
2019-12-08 18:08:02 +0700

    [Update] Join Page & Section data base on service section

--- application/controllers/Service.php
+++ application/controllers/Service.php
@@ -56,13 +56,12 @@ class Service extends MY_Controller
 
 		$service = $this->service_model->get_service($lang, $service_slug);
 		$sections = $this->crud_model->select('service_section', QUERY_RESULT, ['service_section.deleted_at', 'service_section.service_id','section.section_id', 'section_slug', '(SELECT dictionary_content FROM dictionary WHERE dictionary.dictionary_slug=section_menu_name AND dictionary.language_code="' . $lang . '") as section_menu_name', '(SELECT dictionary_content FROM dictionary WHERE dictionary.dictionary_slug=section_name AND dictionary.language_code="' . $lang . '") as section_name'], ['service_section.service_id' => $service->service_id, 'service_section.deleted_at' => null], ['service_section' => ['section' => 'section_id']]);
-
-		$data['title'] = '';
+		$data['title'] = $service->service_name;
 		$data['id'] = 'service';
 		$data['subtitle'] = 'information';
-		$data['data_mode'] = 'general';
+		$data['data_mode'] = 'service';
 		$data['service_id'] = $service_slug;
-		$data['service_name'] = str_replace('-', ' ', lang($data['service_id']));
+		$data['service_name'] = $service->service_name;
 		$data['page_heading'] = $data['service_name'];
 		$data['is_bilingual'] = true;
 		$data['breadcrumb'] = [
@@ -84,18 +83,53 @@ class Service extends MY_Controller
 		];
 
 		$data['subnav'] = $sections;
+		foreach ($sections as $section) {
+			if ($section->section_slug=='our-achievement' || $section->section_slug=='achievement-deliverable'){
+				$data['achievements'] = $this->crud_model->select('achievement', QUERY_RESULT, ['achievement_count', '(SELECT dictionary_content FROM dictionary WHERE dictionary_slug=achievement_name AND language_code="' . $lang . '" limit 1) as achievement_name'], ['achievement.service_id' => 1, 'achievement.deleted_at' => null]);
+			}
+			if ($section->section_slug=='we-offer'){
+
+			}
+			if ($section->section_slug=='activities'){
+
+			}
+			if ($section->section_slug=='benefit' || $section->section_slug=='output-benefit'){
+				$data['benefits'] = $this->crud_model->select('benefit', QUERY_RESULT, ['benefit_icon', '(SELECT dictionary_content FROM dictionary WHERE dictionary_slug=benefit_name AND language_code="' . $lang . '" limit 1) as benefit_name'], ['service_id' => $service->service_id, 'deleted_at' => null]);
+			}
+			if ($section->section_slug=='expertise-and-experience'){
+				$data['expertises'] = $this->crud_model->select('expertise', QUERY_RESULT, ['(SELECT dictionary_content FROM dictionary WHERE dictionary_slug=expertise_name AND language_code="' . $lang . '" limit 1) as expertise_name'], ['service_id' => $service->service_id, 'deleted_at' => null]);
+			}
+			if ($section->section_slug=='what-you-get'){
+				$data['facilities'] = $this->crud_model->select('facility', QUERY_RESULT, ['facility_icon', '(SELECT dictionary_content FROM dictionary WHERE dictionary_slug=facility_name AND language_code="' . $lang . '" limit 1) as facility_name'], ['service_id' => $service->service_id, 'deleted_at' => null]);
+			}
+			if ($section->section_slug=='waste-flow'){
+				$data['flows'] = $this->crud_model->select('flow', QUERY_RESULT, ['flow_icon', '(SELECT dictionary_content FROM dictionary WHERE dictionary_slug=flow_name AND language_code="' . $lang . '" limit 1) as flow_name'], ['service_id' => $service->service_id, 'deleted_at' => null]);
+			}
+			if ($section->section_slug=='waste-flow'){
+
+			}
+			if ($section->section_slug=='photofolio-highlight'){
+				$data['photofolios'] = $this->crud_model->select('photofolio', QUERY_RESULT, '', ['service_id' => $service->service_id, 'deleted_at' => null]);
+			}
+			if ($section->section_slug=='research-highlight' || $section->section_slug=='research-highlight'){
+				$data['projects'] = $this->crud_model->select('project', QUERY_RESULT, ['project_slug','project_client','project_thumbnail','(SELECT dictionary_content FROM dictionary WHERE dictionary_slug=project_name AND language_code="' . $lang . '" limit 1) as project_name','(SELECT dictionary_content FROM dictionary WHERE dictionary_slug=project_category AND language_code="' . $lang . '" limit 1) as project_category'], ['service_id' => $service->service_id, 'deleted_at' => null]);
+			}
+			if ($section->section_slug=='similar-project'){
+
+			}
+			if ($section->section_slug=='our-client'){
+				$data['clients'] = $this->crud_model->select('client', QUERY_RESULT, '', ['service_id' => $service->service_id, 'deleted_at' => null]);
+			}
+			if ($section->section_slug=='recomended-for'){
+				$data['recomendations'] = $this->crud_model->select('service_recomendation', QUERY_RESULT, ['recomendation_icon', 'recomendation_color', '(SELECT dictionary_content FROM dictionary WHERE dictionary_slug=recomendation_name AND language_code="' . $lang . '" limit 1) as recomendation_name'], ['service_recomendation.service_id' => $service->service_id, 'service_recomendation.deleted_at' => null], ['service_recomendation' => ['recomendation' => 'recomendation_id']]);
+			}
+			if ($section->section_slug=='our-coverage'){
+
+			}
+		}
 
 		$data['service'] = $service;
 		$data['sections'] = $sections;
-		$data['achievements'] = $this->crud_model->select('achievement', QUERY_RESULT, ['achievement_count', '(SELECT dictionary_content FROM dictionary WHERE dictionary_slug=achievement_name AND language_code="' . $lang . '" limit 1) as achievement_name'], ['achievement.service_id' => 1, 'achievement.deleted_at' => null]);
-		$data['expertises'] = $this->crud_model->select('expertise', QUERY_RESULT, ['(SELECT dictionary_content FROM dictionary WHERE dictionary_slug=expertise_name AND language_code="' . $lang . '" limit 1) as expertise_name'], ['service_id' => $service->service_id, 'deleted_at' => null]);
-		$data['benefits'] = $this->crud_model->select('benefit', QUERY_RESULT, ['benefit_icon', '(SELECT dictionary_content FROM dictionary WHERE dictionary_slug=benefit_name AND language_code="' . $lang . '" limit 1) as benefit_name'], ['service_id' => $service->service_id, 'deleted_at' => null]);
-		$data['facilities'] = $this->crud_model->select('facility', QUERY_RESULT, ['facility_icon', '(SELECT dictionary_content FROM dictionary WHERE dictionary_slug=facility_name AND language_code="' . $lang . '" limit 1) as facility_name'], ['service_id' => $service->service_id, 'deleted_at' => null]);
-		$data['flows'] = $this->crud_model->select('flow', QUERY_RESULT, ['flow_icon', '(SELECT dictionary_content FROM dictionary WHERE dictionary_slug=flow_name AND language_code="' . $lang . '" limit 1) as flow_name'], ['service_id' => $service->service_id, 'deleted_at' => null]);
-		$data['photofolios'] = $this->crud_model->select('photofolio', QUERY_RESULT, '', ['service_id' => $service->service_id, 'deleted_at' => null]);
-		$data['projects'] = $this->crud_model->select('project', QUERY_RESULT, ['project_slug','project_client','project_thumbnail','(SELECT dictionary_content FROM dictionary WHERE dictionary_slug=project_name AND language_code="' . $lang . '" limit 1) as project_name','(SELECT dictionary_content FROM dictionary WHERE dictionary_slug=project_category AND language_code="' . $lang . '" limit 1) as project_category'], ['service_id' => $service->service_id, 'deleted_at' => null]);
-		$data['clients'] = $this->crud_model->select('client', QUERY_RESULT, '', ['service_id' => $service->service_id, 'deleted_at' => null]);
-		$data['recomendations'] = $this->crud_model->select('service_recomendation', QUERY_RESULT, ['recomendation_icon', 'recomendation_color', '(SELECT dictionary_content FROM dictionary WHERE dictionary_slug=recomendation_name AND language_code="' . $lang . '" limit 1) as recomendation_name'], ['service_recomendation.service_id' => $service->service_id, 'service_recomendation.deleted_at' => null], ['service_recomendation' => ['recomendation' => 'recomendation_id']]);
 
 		$this->render_page('services/details/index', $data, 'services');
 	}
@@ -150,7 +184,7 @@ class Service extends MY_Controller
 		$this->render_page('services/details/index', $data, 'services');
 	}
 
-	public function text()
+	public function test()
 	{
 
 		$service_target = $this->crud_model->select('service_target', QUERY_RESULT, ['service_target_id', 'dictionary.dictionary_content service_target_name', 'service_target_icon'], ['language_code' => $lang, 'deleted_at' => null], ['service_target' => ['dictionary' => 'dictionary_slug=service_target_name']]);
@@ -172,60 +206,26 @@ class Service extends MY_Controller
 		die();
 	}
 
-	public function join($param)
+	public function join($service_slug)
 	{
-		$data['title'] = '';
+		$lang = $this->get_language();
+
+		$service = $this->service_model->get_service($lang, $service_slug);
+		$sections = $this->crud_model->select('service_section', QUERY_RESULT, ['service_section.deleted_at', 'service_section.service_id','section.section_id', 'section_slug', '(SELECT dictionary_content FROM dictionary WHERE dictionary.dictionary_slug=section_menu_name AND dictionary.language_code="' . $lang . '") as section_menu_name', '(SELECT dictionary_content FROM dictionary WHERE dictionary.dictionary_slug=section_name AND dictionary.language_code="' . $lang . '") as section_name'], ['service_section.service_id' => $service->service_id, 'service_section.deleted_at' => null], ['service_section' => ['section' => 'section_id']]);
+
+		$data['title'] = $service->service_name;
 		$data['id'] = 'service';
 		$data['subtitle'] = 'information';
-		$data['data_mode'] = 'general';
-		$data['service_id'] = $param;
-		$data['service_name'] = str_replace('-', ' ', lang($data['service_id']));
+		$data['data_mode'] = 'service';
+		$data['service_id'] = $service_slug;
+		$data['service_name'] = $service->service_name;
 		$data['page_heading'] = $data['service_name'];
-		$data['is_bilingual'] = false;
+		$data['is_bilingual'] = true;
 
-		$data['subnav'] = 'custom';
+		$data['subnav'] = $sections;
+		$data['service'] = $service;
 
-
-		$data['subnav'] = [
-			[
-				'title' => lang('about_general'),
-				'url' => '#about',
-				'type' => 'link',
-			],
-			[
-				'title' => lang('solution'),
-				'url' => '#feature',
-				'type' => 'link',
-			],
-			[
-				'title' => lang('facility'),
-				'url' => '#facility',
-				'type' => 'link',
-			],
-			[
-				'title' => lang('client_only_title'),
-				'url' => '#client',
-				'type' => 'link',
-			],
-			[
-				'title' => lang('recomended_user'),
-				'url' => '#recomended_for',
-				'type' => 'link',
-			],
-			[
-				'title' => lang('get_started'),
-				'url' => '#cta',
-				'type' => 'button',
-				'option' => [
-					'btn-color' => 'btn-info',
-					'icon' => 'fa fa-chevron-right',
-					'option-class' => 'd-none animated bounceInRight toggle-hide',
-				],
-			],
-		];
-
-
-		$this->render_page('services/' . $param . '/join', $data, 'general');
+		$this->render_page('services/join', $data, 'services');
 	}
 
 }
--- application/views/services/join.php
+++ application/views/services/join.php
@@ -1,18 +1,25 @@
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="form-group g-mb-20 text-center" style="padding: 15px 8px">
-                <small class="form-text text-muted g-font-size-default g-mt-10"><?= lang('brand_join_subtitle') ?></small>
-                <small class="form-text text-muted g-font-size-default g-mt-10"><?= lang('brand_join_subtitle2') ?></small>
-                <br>
-                <br>
-                <script>
-                    $(window).ready(function () {
-                        $('#gform').css('width', window.innerWidth-window.innerWidth*2*(10/100));
-                    })
-                </script>
-                <iframe id="<?= $this->agent->is_mobile() ? 'gform' : '' ?>" style="height: <?= $this->agent->is_mobile() ? '1100px' : '1200px' ?>;width: 100%;border: none" src="https://docs.google.com/forms/d/e/1FAIpQLSdRpCd3S-0LXdCZkse5WIHhIStiSHrvTWF34qcertJZBCyoYQ/viewform?embedded=true">Memuat...</iframe>
-            </div>
-        </div>
-    </div>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<div class="row justify-content-center">
+		<div class="col-md-8">
+			<div class="form-group g-mb-20 text-center" style="padding: 15px 8px">
+				<h3 class=""><?= get_lang('join-the-program') ?></h3>
+				<h2 class="text-uppercase"><?= $service->service_name ?></h2>
+				<br>
+				<br>
+				<script>
+            $(window).ready(function () {
+                $('#gform').css('width', window.innerWidth-window.innerWidth*2*(10/100));
+            })
+				</script>
+				<iframe id="<?= $this->agent->is_mobile() ? 'gform' : '' ?>" style="height: <?= $this->agent->is_mobile() ? '1100px' : '1200px' ?>;width: 100%;border: none" src="<?=$service->service_proposal_url?>?embedded=true">Memuat...</iframe>
+			</div>
+		</div>
+	</div>
 </div>
