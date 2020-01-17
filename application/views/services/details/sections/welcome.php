3fd2726a4c1b587d1ddb921e28736a7b4dfa1d6b
Rangga Satria A <ranggasatriaa@gmail.com>
2020-01-17 23:24:39 +0700

    [Update] Welcome section mobile bg color

--- application/views/services/details/sections/welcome.php
+++ application/views/services/details/sections/welcome.php
@@ -1,8 +1,8 @@
 <!-- Promo Block -->
 <section>
 	<div id="<?= $section_slug ?>" class="dzsparallaxer auto-init height-is-based-on-content use-loading mode-scroll loaded dzsprx-readyall " data-options='{direction: "reverse", settings_mode_oneelement_max_offset: "150"}'>
-		<div class="divimage dzsparallaxer--target w-100 g-bg-pos-bottom-center" style="height: 100%;background: url(<?= $this->agent->is_mobile() ? base_url('assets/img/service/' . $service_id . '/bg/header.jpg') : base_url('assets/img/service/' . $service_id . '/bg/header.jpg') ?>);background-size: cover;<?= $this->agent->is_mobile() ? 'background-position-x: right;' : '' ?>"></div>
-		<div class="full-height-block pending-show animated d-zap" style="background-image: linear-gradient(<?= $this->agent->is_mobile() ? '190deg' : '100deg' ?>, rgb(256, 256, 256) 40%, rgba(256, 256, 256, <?= $this->agent->is_mobile() ? '0.85' : '0.85' ?>) 70%, rgba(256, 256, 256, <?= $this->agent->is_mobile() ? '0' : '0' ?>) 100%);">
+		<div class="divimage dzsparallaxer--target w-100 g-bg-pos-bottom-center" style="height: 100%;background: url(<?= base_url('assets/img/service/' . $service_id . '/bg/header.jpg') ?>);background-size: cover;<?= $this->agent->is_mobile() ? 'background-position-x: right;' : '' ?>"></div>
+		<div class="full-height-block pending-show animated d-zap" style="background-image: <?= $this->agent->is_mobile() ? 'linear-gradient(180deg, rgba(255, 255, 255, 0.7) 40%, rgba(255, 255, 255, 0.8) 60%, rgba(255, 255, 255, 0.5) 100%)' : 'linear-gradient( 100deg , rgb(256, 256, 256) 40%, rgba(256, 256, 256, 0.85) 70%, rgba(256, 256, 256,0) 100%);' ?>">
 			<div class="container">
 				<div class="row content-middle-fullscreen with-header <?= $this->agent->is_mobile() ? 'justify-content-center' : '' ?>">
 					<?php if (!$this->agent->is_mobile()): ?>
